a22d5682a3c8b267467513cad7c57053b9739194
unsubstantiated-Script <jusfulton@gmail.com>
2021-10-01 16:30:59 -0700

    problems

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,10 +15,10 @@ exports.onCreateWebpackConfig = ({ stage, actions }) => {
   if (stage.startsWith('develop')) {
     actions.setWebpackConfig({
       resolve: {
+        modules: [path.resolve(__dirname, 'src'), 'node_modules'],
         alias: {
           'react-dom': '@hot-loader/react-dom',
         },
-        modules: [path.resolve(__dirname, 'src'), 'node_modules'],
       },
     });
   }
