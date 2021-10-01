3141074b3cb2611dca80ddf0ceaa972d80a4579a
unsubstantiated-Script <jusfulton@gmail.com>
2021-10-01 14:13:53 -0700

    Image selector setup done

--- src/templates/ProductTemplate/index.js
+++ src/templates/ProductTemplate/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { Layout, ImageGallery } from 'components';
+import { Grid } from './styles';
 
 //This is a page query for one specific product
 export const query = graphql`
@@ -7,14 +9,36 @@ export const query = graphql`
   query ProductQuery($shopifyId: String) {
     shopifyProduct(shopifyId: { eq: $shopifyId }) {
       title
+      description
+      images {
+        id
+        localFile {
+          childImageSharp {
+            fluid(maxWidth: 300) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
     }
   }
 `;
 
 //data from above query is injected below into props.data
 const ProductTemplate = props => {
-  console.log(props);
-  return <h1>{props.data.shopifyProduct.title}</h1>;
+  return (
+    <Layout>
+      <Grid>
+        <div>
+          <h1>{props.data.shopifyProduct.title}</h1>
+          <p>{props.data.shopifyProduct.description}</p>
+        </div>
+        <div>
+          <ImageGallery images={props.data.shopifyProduct.images} />
+        </div>
+      </Grid>
+    </Layout>
+  );
 };
 
 export default ProductTemplate;
