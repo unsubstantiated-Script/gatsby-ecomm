76ded743e28a4078640a32dada696d473370a04c
unsubstantiated-Script <jusfulton@gmail.com>
2021-10-14 11:48:38 -0700

    Wrapping up netlify custom function

--- functions/product-update.js
+++ functions/product-update.js
@@ -1,5 +1,6 @@
 const faunadb = require('faunadb');
 const verifyWebhookIntegrity = require('shopify-verify-webhook');
+const axios = require('axios');
 
 const q = faunadb.query;
 const client = new faunadb.Client({
@@ -15,10 +16,58 @@ exports.handler = function (event, context, callback) {
   );
 
   if (isValid) {
-    callback(null, {
-      statusCode: 200,
-      body: 'Hello Potato',
+    const body = JSON.parse(event.body);
+    const { id } = body;
+    //Removing unnecessary data
+    delete body.updated_at;
+
+    //Cleaning up the product data
+    body.variants.forEach(variant => {
+      delete variant.updated_at;
+      delete variant.inventory_quantity;
+      delete variant.old_inventory_quantity;
     });
+    const bodyString = JSON.stringify(body);
+
+    //Checking to see if this product is in FaunaDB
+    client
+      .query(q.Get(q.Match(q.Index('product_by_id'), id)))
+      .then(result => {
+        if (result.data.product !== bodyString) {
+          //updating faunaDB
+          client
+            .query(
+              q.Update(result.ref, {
+                data: {
+                  product: bodyString,
+                },
+              })
+            )
+            .then(() => {
+              //Calling Rebuild
+              axios.post(process.env.NETLIFY_BUILD_URL);
+            })
+            .catch(e => {
+              console.log('Error updating FaunaDB Item', e);
+            });
+        }
+      })
+      .catch(() => {
+        //If the product doesn't exist in faunaDB, here is where we add it
+        client
+          .query(
+            q.Create(q.Collection('products'), {
+              data: { id, product: body.string },
+            })
+          )
+          .then(() => {
+            //Calling rebuild
+            axios.post(process.env.NETLIFY_BUILD_URL);
+          })
+          .catch(e => {
+            console.log('Error trying to add new item to FaunaDB', e);
+          });
+      });
   } else {
     callback(null, {
       statusCode: 403,
