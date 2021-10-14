5885c6bddf52df38d8d5c9c904049b7aae9086c0
unsubstantiated-Script <jusfulton@gmail.com>
2021-10-14 12:03:55 -0700

    fixing build issues

--- functions/product-update.js
+++ functions/product-update.js
@@ -7,7 +7,6 @@ const client = new faunadb.Client({
   secret: process.env.FAUNADB_SECRET,
 });
 
-//Passing functions to the cloud. An endpoint function
 exports.handler = function (event, context, callback) {
   const isValid = verifyWebhookIntegrity(
     process.env.SHOPIFY_WEBHOOK_KEY,
@@ -18,54 +17,47 @@ exports.handler = function (event, context, callback) {
   if (isValid) {
     const body = JSON.parse(event.body);
     const { id } = body;
-    //Removing unnecessary data
     delete body.updated_at;
-
-    //Cleaning up the product data
     body.variants.forEach(variant => {
       delete variant.updated_at;
       delete variant.inventory_quantity;
       delete variant.old_inventory_quantity;
     });
+
     const bodyString = JSON.stringify(body);
 
-    //Checking to see if this product is in FaunaDB
     client
       .query(q.Get(q.Match(q.Index('product_by_id'), id)))
       .then(result => {
         if (result.data.product !== bodyString) {
-          //updating faunaDB
           client
             .query(
               q.Update(result.ref, {
-                data: {
-                  product: bodyString,
-                },
+                data: { product: bodyString },
               })
             )
             .then(() => {
-              //Calling Rebuild
+              // call rebuild
               axios.post(process.env.NETLIFY_BUILD_URL);
             })
             .catch(e => {
-              console.log('Error updating FaunaDB Item', e);
+              console.log('error updating product: ', e);
             });
         }
       })
       .catch(() => {
-        //If the product doesn't exist in faunaDB, here is where we add it
         client
           .query(
             q.Create(q.Collection('products'), {
-              data: { id, product: body.string },
+              data: { id, product: bodyString },
             })
           )
           .then(() => {
-            //Calling rebuild
+            // call rebuild
             axios.post(process.env.NETLIFY_BUILD_URL);
           })
           .catch(e => {
-            console.log('Error trying to add new item to FaunaDB', e);
+            console.log('error adding to db: ', e);
           });
       });
   } else {
